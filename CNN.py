034ae08a8415257d234b9fb3676514081a935791
Eric Rincon <eric.rincon147@gmail.com>
2016-05-01 23:57:58 -0500

    first commit

---
+++ CNN.py
@@ -0,0 +1,104 @@
+from keras.layers import Input,Embedding, merge, Dense
+
+from keras.models import Model
+
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.core import Flatten, Activation
+from keras.optimizers import Adam, SGD
+
+import sklearn.metrics as metrics
+import numpy as np
+
+class CNN():
+    def __init__(self, n_classes, max_words, w2v_size, vocab_size, use_embedding, filter_sizes, n_filters,
+                 dense_layer_sizes, name, activation_function):
+        self.model = self.build_model(n_classes, max_words, w2v_size, vocab_size, use_embedding, filter_sizes,
+                                      n_filters, dense_layer_sizes, activation_function)
+        self.model_name = name
+
+    def build_model(self, n_classes, max_words, w2v_size, vocab_size, use_embedding, filter_sizes, n_filters,
+                    dense_layer_sizes, activation_function):
+
+        if use_embedding:
+            w2v_input = Input(shape=(max_words, ))
+            conv_input = Embedding(output_dim=w2v_size, input_dim=vocab_size, input_length=max_words)(w2v_input)
+        else:
+            w2v_input = Input(shape=(1, max_words, w2v_size))
+            conv_input = w2v_input
+        conv_layers = []
+
+        for filter_size in filter_sizes:
+            convolution_layer = Convolution2D(n_filters, filter_size, w2v_size,
+                                              input_shape=(1, max_words, w2v_size))(conv_input)
+
+            max_layer = MaxPooling2D(pool_size=(max_words - filter_size + 1, 1))(convolution_layer)
+            activation_layer = Activation(activation_function)(max_layer)
+            flattened_layer = Flatten()(activation_layer)
+
+            conv_layers.append(flattened_layer)
+
+        merge_layer = merge(conv_layers, mode='concat')
+
+        dense_layers = []
+        first_dense_layer = Dense(dense_layer_sizes.pop(0))(merge_layer)
+        dense_layers.append(first_dense_layer)
+
+        if len(dense_layer_sizes) > 1:
+            i = 1
+
+            for dense_layer_size in dense_layer_sizes:
+                dense_layer = Dense(dense_layer_size)(dense_layer_sizes[i-1])
+                dense_activation_layer = Activation(activation_function)(dense_layer)
+                dense_layers.append(dense_activation_layer)
+                i += 1
+
+        # Add last layer
+        softmax_dense_layer = Dense(n_classes)(dense_layers[-1])
+        softmax_layer = Activation('softmax')(softmax_dense_layer)
+
+        model = Model(input=w2v_input, output=softmax_layer)
+
+        return model
+
+    def train(self, x, y, n_epochs, optim_algo='adam', criterion='categorical_crossentropy', save_model=True):
+
+        if optim_algo == 'adam':
+            optim_algo = Adam()
+        else:
+            optim_algo = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+
+        self.model.compile(optimizer=optim_algo, loss=criterion)
+
+        self.model.fit(x, y, nb_epoch=n_epochs)
+
+        if save_model:
+            self.model.save_weights(self.model_name + '.h5', overwrite=True)
+
+    def test(self, x, y):
+        truth = []
+#        if self.k_output == self.window_size:
+
+        predictions = self.predict_classes(x)
+
+
+        for i in range(y.shape[0]):
+            if y[i, 1] == 1:
+                truth.append(1)
+            else:
+                truth.append(0)
+        truth = y
+        accuracy = metrics.accuracy_score(truth, predictions)
+        f1_score = metrics.f1_score(truth, predictions)
+        precision = metrics.precision_score(truth, predictions)
+        auc = metrics.roc_auc_score(truth, predictions)
+        recall = metrics.recall_score(truth, predictions)
+
+        return accuracy, f1_score, precision, auc, recall
+
+    # Graph model uses a dictionary as input so wrap the Keras function to makes it easier to
+    # use!
+    def predict_classes(self, x):
+        predictions = self.model.predict(x)
+        predicted_classes = np.argmax(predictions, axis=1)
+
+        return predicted_classes
---
+++ Experiment.py
@@ -0,0 +1,170 @@
+import getopt
+import sys
+import os
+import pandas as pd
+import nltk
+import numpy as np
+from CNN import CNN
+
+from sklearn.cross_validation import KFold
+from gensim.models import Word2Vec
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], '', ['window_size=', 'wiki=', 'n_feature_maps=', 'epochs=',
+                                                      'undersample=', 'n_feature_maps=', 'criterion=',
+                                                      'optimizer=', 'model=', 'genia=', 'tacc=', 'layers=',
+                                                      'hyperopt=', 'model_name='])
+    except getopt.GetoptError as error:
+        print(error)
+        sys.exit(2)
+
+    w2v_path = '/Users/ericrincon/PycharmProjects/Deep-PICO/wikipedia-pubmed-and-PMC-w2v.bin'
+    model_type = 'nn'
+    window_size = 5
+    wiki = True
+    n_feature_maps = 100
+    epochs = 20
+    undersample = False
+    binary_cross_entropy = False
+    criterion = 'categorical_crossentropy'
+    optimizer = 'adam'
+    k = 2
+    use_genia = False
+    using_tacc = False
+    layer_sizes = []
+    hyperopt = False
+    model_name = 'model'
+    w2v_size = 200
+    activation = 'relu'
+    dense_sizes = [100]
+    filter_sizes = [2, 3, 4, 5]
+
+
+    for opt, arg in opts:
+        if opt == '--window_size':
+            window_size = int(arg)
+        elif opt == '--wiki':
+            if arg == 0:
+                wiki = False
+        elif opt == '--epochs':
+            epochs = int(arg)
+        elif opt == '--layers':
+            layer_sizes = arg.split(',')
+        elif opt == '--n_feature_maps':
+            n_feature_maps = int(arg)
+        elif opt == '--n_feature_maps':
+            n_feature_maps = int(arg)
+        elif opt == '--criterion':
+            criterion = arg
+        elif opt == '--optimizer':
+            optimizer = arg
+        elif opt == '--tacc':
+            if int(arg) == 1:
+                using_tacc = True
+        elif opt == '--hyperopt':
+            if int(arg) == 1:
+                hyperopt = True
+        elif opt == '--model_name':
+            model_name = arg
+        else:
+            print("Option {} is not valid!".format(opt))
+    print('Loading Word2Vec...')
+    w2v = Word2Vec.load_word2vec_format(w2v_path, binary=True)
+    print('Loaded Word2Vec...')
+
+    print('Loading data...')
+    X, y = get_data(50, 200, w2v)
+    print('Loaded data...')
+    n = X.shape[0]
+    kf = KFold(n, random_state=1337, shuffle=True, n_folds=5)
+
+    for fold_idx, (train, test) in enumerate(kf):
+        X_train, y_train = X[train, :, :, :], y[train, :]
+        X_test, y_test = X[test, :, :, :], y[test, :]
+
+        cnn = CNN(n_classes=2, max_words=50, w2v_size=w2v_size, vocab_size=1000, use_embedding=False, filter_sizes=filter_sizes,
+                  n_filters=n_feature_maps, dense_layer_sizes=dense_sizes.copy(), name=model_name, activation_function=activation)
+
+        cnn.train(X_train, y_train, n_epochs=epochs)
+        accuracy, f1_score, precision, auc, recall = cnn.test(X_test, y_test)
+
+        print("Accuracy: {}".format(accuracy))
+        print("F1: {}".format(f1_score))
+        print("Precision: {}".format(precision))
+        print("AUC: {}".format(auc))
+        print("Recall: {}".format(recall))
+
+
+
+def get_all_files(path):
+    file_paths = []
+
+    for path, subdirs, files in os.walk(path):
+        for name in files:
+
+            # Make sure hidden files do not make into the list
+            if name[0] == '.':
+                continue
+            file_paths.append(os.path.join(path, name))
+    return file_paths
+
+def get_data(max_words, word_vector_size, w2v):
+
+    file_paths = get_all_files('Data')
+
+    abstract_text_df = pd.DataFrame()
+    labels_df = pd.DataFrame()
+
+    for file in file_paths:
+        data_frame = pd.read_csv(file)
+
+        abstract_text = data_frame.iloc[:, 4]
+        labels = data_frame.iloc[:, 6]
+
+        abstract_text_df = pd.concat((abstract_text_df, abstract_text))
+        labels_df = pd.concat((labels_df, labels))
+
+    abstracts_as_words = []
+    labels = []
+    total_words = 0
+
+    for i in range(abstract_text_df.shape[0]):
+        abstract = abstract_text_df.iloc[i, :][0]
+
+        if abstract == 'MISSING':
+            continue
+        else:
+            abstract_as_words = nltk.word_tokenize(abstract)
+            abstracts_as_words.append(abstract_as_words)
+            labels.append(labels_df.iloc[i])
+
+            total_words += len(abstracts_as_words)
+    X = np.zeros((len(abstracts_as_words), 1, max_words , word_vector_size))
+    y = np.zeros((len(labels), 2))
+
+    print(total_words/len(abstracts_as_words))
+
+    for i, (abstract, label) in enumerate(zip(abstracts_as_words, labels)):
+        word_matrix = abstract_to_w2v(abstract, max_words, w2v, word_vector_size)
+        X[i, 0, :, :] = word_matrix
+        y[i, label] = 1
+
+    return X, y
+def abstract_to_w2v(abstract_as_words, max_words, w2v, word_vector_size):
+    word_matrix = np.zeros((max_words, word_vector_size))
+
+    for i, word in enumerate(abstract_as_words):
+        if i == max_words - 1:
+            break
+        try:
+            word_vector = w2v[word]
+            word_matrix[i, word_vector] = word_vector
+        except:
+            continue
+
+    return word_matrix
+
+
+if __name__ == '__main__':
+    main()
