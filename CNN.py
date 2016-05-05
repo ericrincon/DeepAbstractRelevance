e207068f81ba35978fa439cf495ac6093f5ee12c
Eric Rincon <eric.rincon147@gmail.com>
2016-05-05 01:58:17 -0500

    Added a new model type

--- CNN.py
+++ CNN.py
@@ -31,7 +31,6 @@ class CNN:
         if activation_function == 'elu':
             activation_function = ELU(alpha=1.0)
 
-
         if use_embedding:
             w2v_input = Input(shape=(max_words, ))
             conv_input = Embedding(output_dim=w2v_size, input_dim=vocab_size, input_length=max_words)(w2v_input)
@@ -75,7 +74,7 @@ class CNN:
         return model
 
     def train(self, x, y, n_epochs, optim_algo='adam', criterion='categorical_crossentropy', save_model=True,
-              verbose=2, plot=True, tensorBoard_path=''):
+              verbose=2, plot=True, tensorBoard_path='', patience=20):
 
         if optim_algo == 'adam':
             optim_algo = Adam()
@@ -86,12 +85,12 @@ class CNN:
 
         self.model.compile(optimizer=optim_algo, loss=criterion)
 
-        early_stopping = EarlyStopping(monitor='val_loss', patience=10, mode='auto')
+        early_stopping = EarlyStopping(monitor='val_loss', patience=patience, mode='auto')
         tensorBoard = TensorBoard()
 
         # verbose: 0 for no logging to stdout, 1 for progress bar logging, 2 for one log line per epoch.
         self.model.fit(x, y, nb_epoch=n_epochs, callbacks=[early_stopping, tensorBoard], validation_split=0.2,
-                       verbose=verbose, batch_size=16)
+                       verbose=verbose, batch_size=32)
 
         if save_model:
             self.model.save_weights(self.model_name + '.h5', overwrite=True)
@@ -210,7 +209,8 @@ class CICNN:
         early_stopping = EarlyStopping(monitor='val_loss', patience=10, mode='auto')
 
         # verbose: 0 for no logging to stdout, 1 for progress bar logging, 2 for one log line per epoch.
-        self.model.fit(x, y, nb_epoch=n_epochs, callbacks=[early_stopping], validation_split=0.2,
+        # @TODO this is not correct! Fix it!!!!
+        self.model.fit([X_positive, X_negative], [y_positive, y_negative], nb_epoch=n_epochs, callbacks=[early_stopping], validation_split=0.2,
                        verbose=verbose, batch_size=16)
 
         if save_model:
@@ -246,5 +246,129 @@ class CICNN:
 
     def save(self):
         self.model.save_weights(self.model_name)
+
+
+class AbstractCNN:
+    def __init__(self, n_classes, max_words, w2v_size, vocab_size, use_embedding, filter_sizes, n_filters,
+                 dense_layer_sizes, name, activation_function, dropout_p):
+        self.model = self.build_model(n_classes, max_words, w2v_size, vocab_size, use_embedding, filter_sizes,
+                                      n_filters, dense_layer_sizes, activation_function, dropout_p)
+        self.model_name = name
+    def build_conv_node(self, n_feature_maps, max_words, w2v_size, activation_function, filter_sizes, vocab_size,
+                        use_embedding):
+        if use_embedding:
+            w2v_input = Input(shape=(max_words, ))
+            conv_input = Embedding(output_dim=w2v_size, input_dim=vocab_size, input_length=max_words)(w2v_input)
+        else:
+            w2v_input = Input(shape=(1, max_words, w2v_size))
+            conv_input = w2v_input
+        conv_layers = []
+
+        for filter_size in filter_sizes:
+            convolution_layer = Convolution2D(n_feature_maps, filter_size, w2v_size,
+                                              input_shape=(1, max_words, w2v_size))(conv_input)
+
+            max_layer = MaxPooling2D(pool_size=(max_words - filter_size + 1, 1))(convolution_layer)
+            activation_layer = Activation(activation_function)(max_layer)
+            flattened_layer = Flatten()(activation_layer)
+
+            conv_layers.append(flattened_layer)
+
+        merge_layer = merge(conv_layers, mode='concat')
+
+        return merge_layer, w2v_input
+    def build_model(self, n_classes, max_words, w2v_size, vocab_size, use_embedding, filter_sizes, n_feature_maps,
+                    dense_layer_sizes, activation_function, dropout_p):
+
+        # From the paper http://arxiv.org/pdf/1511.07289v1.pdf
+        # Supposed to perform better but lets see about that
+        if activation_function == 'elu':
+            activation_function = ELU(alpha=1.0)
+
+        abstract_node, abstract_input = self.build_conv_node(n_feature_maps, max_words, w2v_size, activation_function, filter_sizes,
+                                             vocab_size, use_embedding)
+        title_node, title_input = self.build_conv_node(n_feature_maps, 20, w2v_size, activation_function, filter_sizes, vocab_size, use_embedding)
+        mesh_node, mesh_input = self.build_conv_node(n_feature_maps, 10, w2v_size, activation_function, filter_sizes, vocab_size, use_embedding)
+
+        merge_layer = merge([abstract_node, title_node, mesh_node], mode='concat')
+
+        dense_layers = []
+        first_dense_layer = Dense(dense_layer_sizes.pop(0))(merge_layer)
+        dense_layers.append(first_dense_layer)
+
+        if len(dense_layer_sizes) > 1:
+            i = 1
+
+            for dense_layer_size in dense_layer_sizes:
+                dense_layer = Dense(dense_layer_size)(dense_layer_sizes[i-1])
+                dense_activation_layer = Activation(activation_function)(dense_layer)
+                dropout_layer = Dropout(dropout_p)(dense_activation_layer)
+                dense_layers.append(dropout_layer)
+                i += 1
+
+        # Add last layer
+        softmax_dense_layer = Dense(n_classes)(dense_layers[-1])
+        softmax_layer = Activation('softmax')(softmax_dense_layer)
+
+        model = Model(input=[abstract_input, title_input, mesh_input], output=softmax_layer)
+
+        return model
+
+    def train(self, X_abstract, X_titles, X_mesh, y, n_epochs, optim_algo='adam', criterion='categorical_crossentropy',
+              save_model=True, verbose=2, plot=True, tensorBoard_path='', patience=20):
+
+        if optim_algo == 'adam':
+            optim_algo = Adam()
+        elif optim_algo == 'sgd':
+            optim_algo = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+        elif optim_algo == 'adagrad':
+            optim_algo = Adagrad()
+
+        self.model.compile(optimizer=optim_algo, loss=criterion)
+
+        early_stopping = EarlyStopping(monitor='val_loss', patience=patience, mode='auto')
+        tensorBoard = TensorBoard()
+
+        # verbose: 0 for no logging to stdout, 1 for progress bar logging, 2 for one log line per epoch.
+        self.model.fit([X_abstract, X_titles, X_mesh], y, nb_epoch=n_epochs, callbacks=[early_stopping, tensorBoard], validation_split=0.2,
+                       verbose=verbose, batch_size=32)
+
+        if save_model:
+            self.model.save_weights(self.model_name + '.h5', overwrite=True)
+
+
+
+
+    def test(self, x, y, print_output=False):
+        truth = []
+        predictions = self.predict_classes(x)
+
+        for i in range(y.shape[0]):
+            if y[i, 1] == 1:
+                truth.append(1)
+            else:
+                truth.append(0)
+
+        if print_output:
+            print('Ground truth: {}'.format(truth))
+            print('Predicted: {}'.format(predictions))
+
+        accuracy = metrics.accuracy_score(truth, predictions)
+        f1_score = metrics.f1_score(truth, predictions)
+        precision = metrics.precision_score(truth, predictions)
+        auc = metrics.roc_auc_score(truth, predictions)
+        recall = metrics.recall_score(truth, predictions)
+
+        return accuracy, f1_score, precision, auc, recall
+
+    def predict_classes(self, x):
+        predictions = self.model.predict(x)
+        predicted_classes = np.argmax(predictions, axis=1)
+
+        return predicted_classes
+
+    def save(self):
+        self.model.save_weights(self.model_name)
+
 
 # Implementation of Modelling, Visualising and Summarising Documents with a Single Convolutional Neural Network
--- Experiment.py
+++ Experiment.py
@@ -4,7 +4,9 @@ import os
 import pandas as pd
 import nltk
 import numpy as np
+
 from CNN import CNN
+from CNN import AbstractCNN
 
 from sklearn.cross_validation import KFold
 from gensim.models import Word2Vec
@@ -14,30 +16,32 @@ from nltk.corpus import stopwords
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], '', ['n_feature_maps=', 'epochs=', 'max_words', 'dropout_p='
+        opts, args = getopt.getopt(sys.argv[1:], '', ['n_feature_maps=', 'epochs=', 'max_words=', 'dropout_p='
                                                       'undersample=', 'n_feature_maps=', 'criterion=',
                                                       'optimizer=', 'max_words=', 'layers=',
-                                                      'hyperopt=', 'model_name=', 'w2v_path=', 'tacc=', 'use_all_date='])
+                                                      'hyperopt=', 'model_name=', 'w2v_path=', 'tacc=', 'use_all_date=',
+                                                      'patience=', 'filter_sizes='])
     except getopt.GetoptError as error:
         print(error)
         sys.exit(2)
 
     w2v_path = '/Users/ericrincon/PycharmProjects/Deep-PICO/wikipedia-pubmed-and-PMC-w2v.bin'
-    n_feature_maps = 100
+    n_feature_maps = 150
     epochs = 50
     criterion = 'categorical_crossentropy'
     optimizer = 'adagrad'
     model_name = 'model'
     w2v_size = 200
     activation = 'elu'
-    dense_sizes = [300]
-    filter_sizes = [3, 5, 7, 9, 11, 15, 20]
-    max_words = 800
+    dense_sizes = [400, 400]
+    filter_sizes = [2, 3, 5, 7, 10]
+    max_words = 200
     word_vector_size = 200
     using_tacc = False
-    undersample = True
+    undersample = False
     use_all_date = False
-    p = .8
+    patience = 20
+    p = .5
 
     for opt, arg in opts:
         if opt == '--window_size':
@@ -67,7 +71,7 @@ def main():
                 hyperopt = True
         elif opt == '--model_name':
             model_name = arg
-        elif opt == 'max_words ':
+        elif opt == '--max_words':
             max_words = int(arg)
         elif opt == '--w2v_path':
             w2v_path = arg
@@ -79,6 +83,12 @@ def main():
         elif opt == '--use_all_data':
             if int(arg) == 1:
                 use_all_date = True
+        elif opt == '--patience':
+            patience = int(arg)
+
+        elif opt == '--undersample':
+            if int(arg) == 0:
+                undersample = False
         else:
             print("Option {} is not valid!".format(opt))
     if using_tacc:
@@ -99,11 +109,11 @@ def main():
         X_list, y_list = get_data_separately(max_words, word_vector_size, w2v)
     print('Loaded data...')
 
-    run_on_all_data(X_list, y_list, model_name, max_words, w2v_size, n_feature_maps, dense_sizes, optimizer, criterion,
-                    epochs, filter_sizes, activation, undersample, p)
+    run(X_list, y_list, model_name, max_words, w2v_size, n_feature_maps, dense_sizes, optimizer, criterion,
+                    epochs, filter_sizes, activation, undersample, p, patience)
 
-def run_on_all_data(X_list, y_list, model_name, max_words, w2v_size, n_feature_maps, dense_sizes, optimizer, criterion, epochs,
-                  filter_sizes, activation, undersample, p):
+def run(X_list, y_list, model_name, max_words, w2v_size, n_feature_maps, dense_sizes, optimizer, criterion, epochs,
+                  filter_sizes, activation, undersample, p, patience):
     for X, y in zip(X_list, y_list):
         n = X.shape[0]
         kf = KFold(n, random_state=1337, shuffle=True, n_folds=5)
@@ -140,20 +150,28 @@ def run_on_all_data(X_list, y_list, model_name, max_words, w2v_size, n_feature_m
             _y = [y_train, y_test]
 
             run_model(_X, _y, model_name, fold_idx, max_words, w2v_size, n_feature_maps, dense_sizes, optimizer, criterion, epochs,
-                      filter_sizes, activation, p)
+                      filter_sizes, activation, p, patience)
 
 
 def run_model(X, y, model_name, fold_idx, max_words, w2v_size, n_feature_maps, dense_sizes, optimizer, criterion, epochs,
-              filter_sizes, activation, p):
+              filter_sizes, activation, p, patience, model_type):
     X_train, X_test = X
     y_train, y_test = y
     print("X_train shape: {}".format(X_train.shape))
     temp_model_name = model_name + '_fold_{}.h5'.format(fold_idx + 1)
-    cnn = CNN(n_classes=2, max_words=max_words, w2v_size=w2v_size, vocab_size=1000, use_embedding=False,
-              filter_sizes=filter_sizes, n_filters=n_feature_maps, dense_layer_sizes=dense_sizes.copy(),
-              name=temp_model_name, activation_function=activation, dropout_p=p)
 
-    cnn.train(X_train, y_train, n_epochs=epochs, optim_algo=optimizer, criterion=criterion, verbose=1)
+    if model_type == 'cnn':
+        cnn = CNN(n_classes=2, max_words=max_words, w2v_size=w2v_size, vocab_size=1000, use_embedding=False,
+                  filter_sizes=filter_sizes, n_filters=n_feature_maps, dense_layer_sizes=dense_sizes.copy(),
+                  name=temp_model_name, activation_function=activation, dropout_p=p)
+
+        cnn.train(X_train, y_train, n_epochs=epochs, optim_algo=optimizer, criterion=criterion, verbose=1,
+                  patience=patience)
+    elif model_type == 'acnn':
+        cnn = AbstractCNN(n_classes=2, max_words=max_words, w2v_size=w2v_size, vocab_size=1000, use_embedding=False,
+                  filter_sizes=filter_sizes, n_filters=n_feature_maps, dense_layer_sizes=dense_sizes.copy(),
+                  name=temp_model_name, activation_function=activation, dropout_p=p)
+        cnn.train()
     accuracy, f1_score, precision, auc, recall = cnn.test(X_test, y_test, print_output=True)
 
     print("Accuracy: {}".format(accuracy))
@@ -177,58 +195,112 @@ def get_all_files(path):
             file_paths.append(os.path.join(path, name))
     return file_paths
 
-def get_data_separately(max_words, word_vector_size, w2v):
+def get_data_separately(max_words, word_vector_size, w2v, use_abstract_cnn=False,
+                        max_mesh_terms=20, max_words_title=12):
     file_paths = get_all_files('Data')
     X_list, y_list = [], []
+    X_mesh_list, X_title_list = [], []
+
     total_words = 0.0
+    total_words_title = 0.0
+    total_mesh_terms = 0.0
+
     n_abstracts = 0.0
 
     for file in file_paths:
-        abstract_text, abstract_labels = extract_abstract_and_labels(file)
+        data_frame = pd.read_csv(file)
+
+        abstract_text, abstract_labels = extract_abstract_and_labels(data_frame)
+        mesh_terms, title = extract_mesh_and_title(data_frame)
 
         abstracts_as_words = []
         labels = []
+        if use_abstract_cnn:
+            abstract_mesh_terms = []
+            titles = []
+
         for i in range(abstract_text.shape[0]):
             abstract = abstract_text.iloc[i]
 
             if abstract == 'MISSING':
                 continue
             else:
-                abstract_as_words = nltk.word_tokenize(abstract)
+                if use_abstract_cnn:
+                    mesh_term = mesh_terms.iloc[i]
+                    abstract_title = title.iloc[i]
 
-                total_words += len(abstracts_as_words)
+                    preprocessed_mesh_terms = preprocess(mesh_term, tokenize=True)
+                    preprocessed_title = preprocess(abstract_title, tokenize=True)
+
+                    total_mesh_terms += len(preprocessed_mesh_terms)
+                    total_mesh_terms += len(preprocessed_title)
+
+                    abstract_mesh_terms.append(preprocessed_mesh_terms)
+                    titles.append(preprocessed_title)
+
+                preprocessed_abstract = preprocess(abstract, tokenize=True)
+
+                total_words += len(preprocessed_abstract)
                 n_abstracts += 1.0
 
-                abstracts_as_words.append(abstract_as_words)
+                abstracts_as_words.append(preprocessed_abstract)
                 labels.append(abstract_labels.iloc[i])
 
-        X = np.zeros((len(abstracts_as_words), 1, max_words, word_vector_size))
+        X = np.empty((len(abstracts_as_words), 1, max_words, word_vector_size))
+
+        if use_abstract_cnn:
+            X_mesh = np.empty((len(abstract_mesh_terms), 1, max_mesh_terms, word_vector_size))
+            X_title = np.empty((len(titles), 1, max_mesh_terms, word_vector_size))
+
         y = np.zeros((len(labels), 2))
 
         for i, (abstract, label) in enumerate(zip(abstracts_as_words, labels)):
-            word_matrix = abstract_to_w2v(abstract, max_words, w2v, word_vector_size)
+            word_matrix = text2w2v(abstract, max_words, w2v, word_vector_size)
+
+            if use_abstract_cnn:
+                mesh_term_matrix = text2w2v(abstract_mesh_terms[i], max_mesh_terms, w2v, word_vector_size)
+                title_matrix = text2w2v(titles[i], max_words_title, w2v, word_vector_size)
+
+                X_mesh[i, 0, :, :] = mesh_term_matrix
+                X_title[i, 0, :, :] = title_matrix
+
             X[i, 0, :, :] = word_matrix
+
             if label == -1:
                 label = 0
             y[i, label] = 1
 
         X_list.append(X)
         y_list.append(y)
+        X_mesh_list.append(X_mesh)
+        X_title_list.append(X_title)
     average_word_per_abstract = float(total_words)/float(n_abstracts)
+    average_words_per_title = float(total_words_title)/float(n_abstracts)
+    average_words_per_mesh = float(total_mesh_terms)/float(n_abstracts)
 
-    print('Average word per abstract: {}'.format(average_word_per_abstract))
+    print('Average words per abstract: {}'.format(average_word_per_abstract))
+    print('Average words per title: {}'.format(average_words_per_title))
+    print('Average words per mesh terms: {}'.format(average_words_per_mesh))
 
-    return X_list, y_list
+    if use_abstract_cnn:
+        return X_list, X_mesh_list, X_title_list, y_list
+    else:
+        return X_list, y_list
 
 
-def extract_abstract_and_labels(file):
-    data_frame = pd.read_csv(file)
-
+def extract_abstract_and_labels(data_frame):
     abstract_text = data_frame.iloc[:, 4]
     labels = data_frame.iloc[:, 6]
 
     return abstract_text, labels
 
+
+def extract_mesh_and_title(data_frame):
+
+    mesh_terms = data_frame.iloc[:, ]
+    titles = data_frame.iloc[:, 5]
+
+    return mesh_terms, titles
 
 def get_data(max_words, word_vector_size, w2v):
 
@@ -259,13 +331,13 @@ def get_data(max_words, word_vector_size, w2v):
             labels.append(labels_df.iloc[i])
 
             total_words += len(abstracts_as_words)
-    X = np.zeros((len(abstracts_as_words), 1, max_words , word_vector_size))
+    X = np.empty((len(abstracts_as_words), 1, max_words , word_vector_size))
     y = np.zeros((len(labels), 2))
 
     print(total_words/len(abstracts_as_words))
 
     for i, (abstract, label) in enumerate(zip(abstracts_as_words, labels)):
-        word_matrix = abstract_to_w2v(abstract, max_words, w2v, word_vector_size)
+        word_matrix = text2w2v(abstract, max_words, w2v, word_vector_size)
         X[i, 0, :, :] = word_matrix
         label = label[0]
         if label == -1:
@@ -275,28 +347,58 @@ def get_data(max_words, word_vector_size, w2v):
     return X, y
 
 
-def abstract_to_w2v(abstract_as_words, max_words, w2v, word_vector_size, remove_stop_words=True):
+def text2w2v(words, max_words, w2v, word_vector_size, remove_stop_words=False):
+
     if remove_stop_words:
         stop = stopwords.words('english')
+    else:
+        stop = None
     i = 0
     word_matrix = np.zeros((max_words, word_vector_size))
 
-    for word in abstract_as_words:
-        if word in stop and remove_stop_words:
-            continue
+    for word in words:
+        if remove_stop_words:
+            if word in stop:
+                print(word)
+                continue
 
         if i == max_words - 1:
             break
         try:
             word_vector = w2v[word]
             word_matrix[i, word_vector] = word_vector
+            print(word_vector)
             i += 1
         except:
             continue
 
+    return word_matrix[np.newaxis, :, :]
 
+def create_whitespace(length):
+    whitespace = ''.join(" " for i in range(length))
 
-    return word_matrix
+    return whitespace
+
+def preprocess(line, tokenize=True, to_lower=True):
+    punctuation = "`~!@#$%^&*()_-=+[]{}\|;:'\"|<>,./?åαβ"
+    numbers = "1234567890"
+    number_replacement = create_whitespace(len(numbers))
+    spacing = create_whitespace(len(punctuation))
+
+    if to_lower:
+        line = line.lower()
+
+    translation_table = str.maketrans(punctuation, spacing)
+    translated_line = line.translate(translation_table)
+    translation_table_numbers = str.maketrans(numbers, number_replacement)
+    final_line = translated_line.translate(translation_table_numbers)
+
+    if tokenize:
+        line_tokens = final_line.split()
+
+        return line_tokens
+    else:
+        return final_line
 
 
 if __name__ == '__main__':
